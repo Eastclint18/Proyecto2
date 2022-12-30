2849eb6eb36478d107cbdf83ad99d2f10671b23b
Eastclint18 <igorivas18@gmail.com>
2022-12-29 22:51:04 -0300

    add: Styles

--- script.js
+++ script.js
@@ -5,8 +5,45 @@ const peliculas = [
     {Nombre:"isla siniestra", Genero:"thriller", Año:'2010'}
 ]
 
-let genero = (prompt('ingresa genero'));
-let filtrados =  peliculas.find(item => item.Genero == genero);
 
-alert(filtrados.Nombre);
+
+
+
+
+let usuario;
+let usuarioStorage = localStorage.getItem("usuario");
+
+
+
+
+if(usuarioStorage){
+    usuario = usuarioStorage;
+    let mensaje = `hola! ${usuario} Que vas a ver hoy?`;
+    let saludo = document.getElementById("saludo");
+   
+    //  saludo.innerHTML = "mensaje";     NO SE PORQUE NO PUDE CAMbiAR  EL iNNER HTML DEL ELEMENT H2 POR ESO LO HICE POR ALERT DESPUES DE HORAS DE INTENTOS>
+    alert(mensaje);
+}else { 
+    usuario = prompt("Hola! Ingresa tu nombre");
+    localStorage.setItem("usuario",usuario);
+    let mensaje = `hola! ${usuario} Que vas a ver hoy?`;
+    alert(mensaje);
+    }
+    
+   
+
+
+document.addEventListener("keyup", e =>{
+
+    if(e.target.matches("#buscador")){
+        document.querySelectorAll(".peli").forEach(peliculas => {
+           
+            peliculas.textContent.toLowerCase().includes(e.target.value.toLowerCase())
+            ?peliculas.classList.remove("filtro")
+            :peliculas.classList.add("filtro")
+       }) 
+    }
+
+
+})
 
